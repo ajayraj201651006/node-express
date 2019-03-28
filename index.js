e9ef3f70b73776767e331325bd85ecbaae91e8ad
ajayraj201651006 <ajayraj003016@gmail.com>
2019-03-28 10:49:04 +0530

    setup express router

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = '3000';
@@ -8,6 +9,31 @@ const port = '3000';
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/dishes', (req, res, next) => {
+   res.end('Will send the all details of dishes to you!');
+});
+
+app.post('/dishes', (req, res, next) => {
+   res.end('Will add the dish: '+ req.body.name +
+   'with details: '+ req.body.description);
+});
+
+app.put('/dishes',  (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported!');
+});
+
+app.delete('/dishes', (req, res, next) => {
+   res.end('Deleting all dishes !');
+});
 
 app.use(express.static(__dirname + '/public'));
 
